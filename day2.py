7bddca65f2c95b91442ccce9b5b9294f789171c0
Saar Sayfan <saarsayfan@gmail.com>
2022-12-22 01:08:07 -0800

    day 2 part 1

---
+++ day2.py
@@ -0,0 +1,29 @@
+##########
+# PART 1 #
+##########
+scoring = {
+    'A': {
+        'X': 1 + 3,
+        'Y': 2 + 6,
+        'Z': 3 + 0,
+    },
+    'B': {
+        'X': 1 + 0,
+        'Y': 2 + 3,
+        'Z': 3 + 6,
+    },
+    'C': {
+        'X': 1 + 6,
+        'Y': 2 + 0,
+        'Z': 3 + 3,
+    },
+}
+
+score = 0
+with open('input/day2.txt', 'r') as f:
+    for line in f.readlines():
+        stripped_line = line.replace('\n', '').strip()
+        their_move, your_move = stripped_line.split(' ')
+        score += scoring[their_move][your_move]
+
+print(f'DAY 2 PART 1:\n{score}\n')
