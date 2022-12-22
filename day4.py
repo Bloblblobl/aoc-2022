1716a25523e23fefdb6da4bfd94207374da4777e
Saar Sayfan <saarsayfan@gmail.com>
2022-12-22 01:14:57 -0800

    day 4 part 1

---
+++ day4.py
@@ -0,0 +1,19 @@
+##########
+# PART 1 #
+##########
+fully_encompassed_ranges = 0
+
+with open('input/day4.txt', 'r') as f:
+    for line in f.readlines():
+        stripped_line = line.replace('\n', '').strip()
+        first_sections, second_sections = stripped_line.split(',')
+        first_start, first_end = first_sections.split('-')
+        second_start, second_end = second_sections.split('-')
+        first_encompasses_second = int(first_start) <= int(
+            second_start) and int(first_end) >= int(second_end)
+        second_encompasses_first = int(first_start) >= int(
+            second_start) and int(first_end) <= int(second_end)
+        if first_encompasses_second or second_encompasses_first:
+            fully_encompassed_ranges += 1
+
+print(f'DAY 4 PART 1:\n{fully_encompassed_ranges}\n')
