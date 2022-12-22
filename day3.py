5ba090f94776c3b733023a95c772d4f27bdc9a04
Saar Sayfan <saarsayfan@gmail.com>
2022-12-22 01:13:08 -0800

    day 3 part 2

--- day3.py
+++ day3.py
@@ -25,3 +25,33 @@ with open('input/day3.txt', 'r') as f:
         total_priority += priority(common_c)
 
 print(f'DAY 3 PART 1:\n{total_priority}\n')
+
+##########
+# PART 2 #
+##########
+total_priority = 0
+first_set = None
+second_set = None
+
+with open('input/day3.txt', 'r') as f:
+    for line in f.readlines():
+        stripped_line = line.replace('\n', '').strip()
+        current_set = set()
+        for c in stripped_line:
+            current_set.add(c)
+
+        if first_set is None:
+            first_set = current_set
+        elif second_set is None:
+            second_set = current_set
+        else:
+            first_second_common = first_set.intersection(second_set)
+            second_third_common = second_set.intersection(current_set)
+            common_set = first_second_common.intersection(
+                second_third_common)
+            common_c = list(common_set)[0]
+            total_priority += priority(common_c)
+            first_set = None
+            second_set = None
+
+print(f'DAY 3 PART 2:\n{total_priority}\n')
