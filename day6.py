ab190bd2d750bebb16ead842f7b97561c3b10096
Saar Sayfan <saarsayfan@gmail.com>
2022-12-22 23:51:34 -0800

    day 6 part 2

--- day6.py
+++ day6.py
@@ -14,3 +14,21 @@ with open('input/day6.txt', 'r') as f:
             break
 
     print(f'DAY 6 PART 1:\n{marker_index}\n')
+
+##########
+# PART 2 #
+##########
+with open('input/day6.txt', 'r') as f:
+    n = 14
+    last_n_seen = []
+    input = f.readlines()[0].replace('\n', '')
+    for i, c in enumerate(input):
+        last_n_seen.append(c)
+        if len(last_n_seen) > n:
+            last_n_seen.pop(0)
+
+        if len(last_n_seen) == n and len(last_n_seen) == len(set(last_n_seen)):
+            marker_index = i + 1
+            break
+
+    print(f'DAY 6 PART 2:\n{marker_index}\n')
