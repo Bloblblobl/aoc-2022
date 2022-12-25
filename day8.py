02317d431b417d54c145ff8051dd4732dc1255a7
Saar <saar@codehs.com>
2022-12-25 01:19:51 -0800

    day 8 part 1

---
+++ day8.py
@@ -0,0 +1,78 @@
+##########
+# PART 1 #
+##########
+lines = []
+with open('input/day8.txt', 'r') as f:
+    for line in f.readlines():
+        lines.append(line.replace('\n', ''))
+
+top_vis_map = {}
+right_vis_map = {}
+bottom_vis_map = {}
+left_vis_map = {}
+w = len(lines[0])
+h = len(lines)
+
+highest_top = 0
+highest_bottom = 0
+for x in range(w):
+    for y in range(h):
+        top_p = (x, y)
+        bottom_p = (x, h - y - 1)
+        top_h = lines[y][x]
+        bottom_h = lines[h - y - 1][x]
+        if y == 0:
+            top_vis_map[top_p] = True
+            bottom_vis_map[bottom_p] = True
+            highest_top = top_h
+            highest_bottom = bottom_h
+        else:
+            if top_h > highest_top:
+                top_vis_map[top_p] = True
+                highest_top = top_h
+            else:
+                top_vis_map[top_p] = False
+            if bottom_h > highest_bottom:
+                bottom_vis_map[bottom_p] = True
+                highest_bottom = bottom_h
+            else:
+                bottom_vis_map[bottom_p] = False
+
+highest_left = 0
+highest_right = 0
+for y in range(h):
+    for x in range(w):
+        left_p = (x, y)
+        right_p = (w - x - 1, y)
+        left_h = lines[y][x]
+        right_h = lines[y][w - x - 1]
+        if x == 0:
+            left_vis_map[left_p] = True
+            right_vis_map[right_p] = True
+            highest_left = left_h
+            highest_right = right_h
+        else:
+            if left_h > highest_left:
+                left_vis_map[left_p] = True
+                highest_left = left_h
+            else:
+                left_vis_map[left_p] = False
+            if right_h > highest_right:
+                right_vis_map[right_p] = True
+                highest_right = right_h
+            else:
+                right_vis_map[right_p] = False
+
+visible_count = 0
+for x in range(w):
+    for y in range(h):
+        p = (x, y)
+        if (
+            top_vis_map[p] or
+            right_vis_map[p] or
+            bottom_vis_map[p] or
+            left_vis_map[p]
+        ):
+            visible_count += 1
+
+print(f'DAY 8 PART 1:\n{visible_count}\n')
