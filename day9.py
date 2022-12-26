b2b03a4cd073e010b7b9a862ac0b3228f828fb26
Saar <saar@codehs.com>
2022-12-25 17:04:32 -0800

    day 9 part 2

--- day9.py
+++ day9.py
@@ -51,3 +51,64 @@ for line in lines:
 
 print(f'DAY 9 PART 1:\n{len(visited)}\n')
 
+##########
+# PART 2 #
+##########
+rope_length = 10
+rope = [[0, 0] for _ in range(rope_length)]
+visited = set([(0,0)])
+
+def adjust_rope(r, last_r):
+    dx, dy = last_r[0] - r[0], last_r[1] - r[1]
+    if dx > 1:
+        r[0] += 1
+        if dy <= -1:
+            r[1] -= 1
+        elif dy >= 1:
+            r[1] += 1
+
+    elif dx < -1:
+        r[0] -= 1
+        if dy <= -1:
+            r[1] -= 1
+        elif dy >= 1:
+            r[1] += 1
+
+    elif dy > 1:
+        r[1] += 1
+        if dx <= -1:
+            r[0] -= 1
+        elif dx >= 1:
+            r[0] += 1
+
+    elif dy < -1:
+        r[1] -= 1
+        if dx <= -1:
+            r[0] -= 1
+        elif dx >= 1:
+            r[0] += 1
+
+for line in lines:
+    d, n = line.split(' ')
+    for _ in range(int(n)):
+        h = rope[0]
+        if d == 'U':
+            h[1] += 1
+
+        elif d == 'R':
+            h[0] += 1
+
+        elif d == 'D':
+            h[1] -= 1
+
+        elif d == 'L':
+            h[0] -= 1
+        
+        for i in range(1, rope_length):
+            adjust_rope(rope[i], rope[i - 1])
+                
+        t = rope[-1]
+        
+        visited.add((t[0], t[1]))
+    
+print(f'DAY 9 PART 2:\n{len(visited)}\n')
