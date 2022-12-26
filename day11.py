33144b38d01111b255200dcae8d95326bb5aaba1
Saar <saar@codehs.com>
2022-12-26 02:07:04 -0800

    day 11 part 2

--- day11.py
+++ day11.py
@@ -47,3 +47,49 @@ for r in range(num_rounds):
 max1, max2 = sorted(inspection_map.values())[-2:]
 result = max1 * max2
 print(f'DAY 11 PART 1:\n{result}\n')
+
+##########
+# PART 2 #
+##########
+lines2 = lines.copy()
+monkeys = []
+common_mod = 1
+while len(lines2):
+    lines2.pop(0)
+    item_line = lines2.pop(0)
+    op_line = lines2.pop(0)
+    test_line = lines2.pop(0)
+    test_true_line = lines2.pop(0)
+    test_false_line = lines2.pop(0)
+
+    items = [int(i) for i in item_line.split(': ')[1].split(', ')]
+    op_text = op_line.split('= ')[1]
+    divisible_by = int(test_line.split(' ')[-1])
+    test_true = int(test_true_line.split(' ')[-1])
+    test_false = int(test_false_line.split(' ')[-1])
+    common_mod *= divisible_by
+    monkeys.append({
+        'items': items,
+        'op_text': op_text,
+        'divisible_by': divisible_by,
+        'test_true': test_true,
+        'test_false': test_false,
+    })
+
+num_rounds = 10000
+inspection_map = {i: 0 for i in range(len(monkeys))}
+for r in range(num_rounds):
+    for i, m in enumerate(monkeys):
+        items = m['items']
+        while len(items):
+            inspection_map[i] += 1
+            item = items.pop(0)
+            op = lambda old: eval(m['op_text'])
+            new_item = op(item) % common_mod
+            passes_test = new_item % m['divisible_by'] == 0
+            next_monkey = m['test_true'] if passes_test else m['test_false']
+            monkeys[next_monkey]['items'].append(new_item)
+
+max1, max2 = sorted(inspection_map.values())[-2:]
+result = max1 * max2
+print(f'DAY 11 PART 2:\n{result}\n')
